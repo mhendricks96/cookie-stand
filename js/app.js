2710c52e3427e0ea9f24c0f156cc3917162f2e91
mhendricks96 <mhendricks96@yahoo.com>
2021-01-27 10:19:36 -0800

    added hours variabl

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('yeah, mike');
 
-
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
 let seattleStore = {
   name: 'seattleStore',
@@ -12,7 +12,8 @@ let seattleStore = {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
+  },
+
 };
 
 
