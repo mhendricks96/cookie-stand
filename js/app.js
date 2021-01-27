31af72d97176225ab866c5960b6a22721d18f209
mhendricks96 <mhendricks96@yahoo.com>
2021-01-27 12:21:36 -0800

    added more to js

--- js/app.js
+++ js/app.js
@@ -5,16 +5,20 @@ let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 let seattleStore = {
   name: 'seattleStore',
+  location: 'Seattle',
   minHourly: 23,
   maxHourly: 65,
   avgPerCustomer: 6.3,
   customersPerHour: function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
   },
-
+  cookiesPerHour: function findCookiesPerHour(){
+    return (this.avgPerCustomer * this.customersPerHour);
+  }
 };
 
 
 console.log(seattleStore);
+console.log(seattleStore.cookiesPerHour);
