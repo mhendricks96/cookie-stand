e430f251f006067768130cd05a1804d0703d3ef5
mhendricks96 <mhendricks96@yahoo.com>
2021-01-28 12:17:49 -0800

    finally finished seattle store by remembering i needed to put it on the html page as well

--- js/app.js
+++ js/app.js
@@ -3,16 +3,17 @@ console.log('yeah, mike');
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
-let myContainer = document.getElementById('container');
+//et myContainer = document.getElementById('container');
 //let section = document.createElement('section');
 let seattleList = document.getElementById('seattle');
 
-let section = document.createElement('section');
-myContainer.appendChild(section);
+
+//let section = document.createElement('section');
+//myContainer.appendChild(section);
 
 
-let article = document.createElement('article');
-section.appendChild(article);
+//let article = document.createElement('article');
+//section.appendChild(article);
 
 
 let seattleStore = {
@@ -43,9 +44,10 @@ let seattleStore = {
   render: function () {
     this.calcCookiesSoldHourly();
     for (let i = 0; i < this.cookiesSoldPerHourArray.length; i++){
-      //let li = document.createElement('li');
-      section.textContent = `${hours[i]}: ${(this.cookiesSoldPerHourArray[i])} cookies`;
-      seattleList.appendChild(section);
+
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${(this.cookiesSoldPerHourArray[i])} cookies`;
+      seattleList.appendChild(li);
     }
     let totalSalesFinal = document.createElement('li');
     totalSalesFinal.textContent = `Total: ${this.dailyTotal} cookies.`;
