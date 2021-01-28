5f2b54c9d2635534cf1d0100a8a6f9d7285ed863
mhendricks96 <mhendricks96@yahoo.com>
2021-01-27 20:47:22 -0800

    added more to js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
 'use strict';
 console.log('yeah, mike');
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
+function randomCustomerPerHour(){
 
-let seattleStore = {
-  name: 'seattleStore',
-  location: 'Seattle',
+};
+
+let seattle = {
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgPerCustomer: 6.3,
-  cookiesSoldPerHourArray: [16, 20, 35, 48]
-  dailyStoreTotal: 0, 
+  cookiesSoldPerHourArray: [],
+  dailyStoreTotal: 0,
   randomCustomersEachHour: function getRandomIntInclusive(min, max) {
-    //return some math here
     return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) + this.minHourlyCustomers);
   },
-  calcCookiesEachHour: function (){
-    let reandomCustomersThisHour = this.randomCustomersEachHour();
-    //for loop
-    //  multiply customer number times avf
+  calcCookiesSoldEachHour: function (){
+    let reandomCustomersforOneHour = this.randomCustomersEachHour();
+    for (var i = 0; i < hours.length; i++)
+    //  multiply customer number times avg
     //  handle the number. round up
     //  push into the cookiesSoldPerHourArray
-  },
-  render: function (){
-    this.calcCookiesEachHour();
+  
+    render: function (){
+    this.calcCookiesSoldEachHour();
     //do something
   }
-};
+},
 
 
-seattleStore.render();
-parseInt.render();
+seattle.render();
