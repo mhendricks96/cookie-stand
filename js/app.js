61427ec047ba9ddf5b5d17f38d9348b78dbd75c8
mhendricks96 <mhendricks96@yahoo.com>
2021-01-28 10:33:25 -0800

    still working on seattle location

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ console.log('yeah, mike');
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
-
+let seattleList = document.getElementById(seattle);
 
 let seattle = {
   minHourlyCustomers: 23,
@@ -16,10 +16,12 @@ let seattle = {
     return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) + this.minHourlyCustomers);
   },
   calcCookiesSoldHourly: function (){
+    this.randomCustomersEachHour();
     let reandomCookiesSoldHourly = this.randomCustomersEachHour();
     for (let i = 0; i < hours.length; i++){
       //let totalSalesPerHour = Math.floor(this.randomCustomersEachHour)() * this.avgPerCustomer();
-      this.cookiesSoldPerHourArray.push(reandomCookiesSoldHourly * this.avgPerCustomer);
+      this.cookiesSoldPerHourArray.push(Math.ceil(this.randomCustomersEachHour() * this.avgPerCustomer));
+      this.dailyTotal = this.dailyTotal + cookiesSoldPerHourArray[i];
     }
   },
   //  multiply customer number times avg
@@ -28,6 +30,7 @@ let seattle = {
 
   render: function () {
     this.calcCookiesSoldHourly();
+    for (let i = 0)
   },
 };
 
